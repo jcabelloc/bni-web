7fbf6100da5262236252a7f17661074197ddd20e
Guillermo Gavilano <ggavilano@itana.pe>
2020-04-24 15:30:21 -0500

    BNI-3 Actualizar miembro

--- src/app/dialog/save-miembro/save-miembro.component.ts
+++ src/app/dialog/save-miembro/save-miembro.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Miembro } from 'src/app/models/miembro';
 import { AngularFireStorage } from '@angular/fire/storage';
 import { database } from 'firebase';
@@ -14,17 +14,19 @@ export class SaveMiembroComponent implements OnInit {
   miembro: Miembro = new Miembro();
   selectedFile: File;
   imageName: string;
-  constructor(public dialogRef: MatDialogRef<SaveMiembroComponent>) { }
+  constructor(public dialogRef: MatDialogRef<SaveMiembroComponent>, @Inject(MAT_DIALOG_DATA) public data: { miembro: Miembro }) { }
 
   ngOnInit(): void {
-
+    if (this.data.miembro) {
+        this.miembro = this.data.miembro;
+    }
   }
 
   agregarMiembro() {
-    this.dialogRef.close({miembro: this.miembro, imageFile: this.selectedFile})
+    this.dialogRef.close({ miembro: this.miembro, imageFile: this.selectedFile })
   }
 
-  onFileSelected(event:any) {
+  onFileSelected(event: any) {
     this.selectedFile = event.target.files[0] as File;
     this.imageName = this.selectedFile.name;
   }
