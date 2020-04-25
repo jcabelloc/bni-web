8e71e05545511f7419972192655912ed44a18ef0
Guillermo Gavilano <ggavilano@itana.pe>
2020-04-25 10:40:26 -0500

    BNI-3 Implementacion de la búsqueda por filtros

--- src/app/pages/adm-miembros/adm-miembros.component.ts
+++ src/app/pages/adm-miembros/adm-miembros.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Miembro } from 'src/app/models/miembro';
 import { SaveMiembroComponent } from 'src/app/dialog/save-miembro/save-miembro.component';
 import { MatDialog } from '@angular/material/dialog';
-
 import { MiembroService } from 'src/app/services/miembro.service';
+
+interface Chip {
+  key: string;
+  value: string;
+}
 
 @Component({
   selector: 'app-adm-miembros',
@@ -16,11 +20,19 @@ export class AdmMiembrosComponent implements OnInit {
   displayedColumns: string[] = ['avatar', 'nombre', 'profesion', 'telefono', 'email', 'nombreCompania', 'acciones'];
 
   miembros: Miembro[]
-  showFilters: boolean = true;
+  cloneMiembros: Miembro[]
+  showFilters: boolean = false;
+  filtros: Chip[] = new Array<Chip>();
+  nombreFiltro: string;
+  companiaFiltro: string;
+  profesionFiltro: string;
   constructor(private miembroService: MiembroService, private dialog: MatDialog, public snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
 
+    this.getMiembros();
+  }
+  getMiembros() {
     this.miembroService.getMiembros().subscribe(
       miembros => {
         this.miembros = miembros;
@@ -30,11 +42,11 @@ export class AdmMiembrosComponent implements OnInit {
               .subscribe(avatarUrl => {
                 miembro.avatarUrl = avatarUrl
               }));
+        this.cloneMiembros = this.miembros;
+        this.filterMiembros();
       }
     );
-
   }
-
   addMiembro() {
     const dialogRef = this.dialog.open(SaveMiembroComponent, { width: '800px' });
     dialogRef.afterClosed().subscribe(data => {
@@ -45,7 +57,7 @@ export class AdmMiembrosComponent implements OnInit {
               (() => {
                 data.miembro.idMiembro = idMiembro;
                 this.getAvatarUrlMiembro(data.miembro);
-              }, err =>this.snackBar.open(err, '', { duration: 2000 }));
+              }, err => this.snackBar.open(err, '', { duration: 2000 }));
 
           },
           err => this.snackBar.open(err, '', { duration: 2000 }))
@@ -79,5 +91,85 @@ export class AdmMiembrosComponent implements OnInit {
 
   deleteMiembro(id: string) {
     this.miembroService.deleteMiembro(id);
+  }
+
+  filterMiembros() {
+    let nombre = "Nombre";
+    let compania = "Compañia";
+    let profesion = "Profesion";
+    this.miembros = this.cloneMiembros;
+    this.filtros.forEach(filtro => {
+      switch (filtro.key) {
+        case nombre:
+          this.miembros = this.miembros.filter(miembro => {
+            let nombreCompleto = miembro.nombres + " " + miembro.apellidos;
+            if (nombreCompleto.toLowerCase().includes(filtro.value.toLowerCase())) {
+              return true;
+            }
+          })
+          break;
+        case compania:
+          this.miembros = this.miembros.filter(miembro => miembro.nombreCompania.toLowerCase().includes(filtro.value.toLowerCase()));
+          break;
+        case profesion:
+          this.miembros = this.miembros.filter(miembro => miembro.profesion.toLowerCase().includes(filtro.value.toLowerCase()));
+          break;
+      }
+    });
+  }
+
+  addFiltroNombre() {
+    let indexFiltro = this.existFiltro("Nombre");
+    if (indexFiltro == -1) {
+      this.filtros.push({ key: "Nombre", value: this.nombreFiltro })
+    } else {
+      this.filtros[indexFiltro].value = this.nombreFiltro;
+    }
+    this.nombreFiltro = null;
+    this.filterMiembros();
+  }
+
+  addFiltroCompania() {
+    let indexFiltro = this.existFiltro("Compañia");
+    if (indexFiltro == -1) {
+      this.filtros.push({ key: "Compañia", value: this.companiaFiltro })
+    } else {
+      this.filtros[indexFiltro].value = this.companiaFiltro;
+    }
+    this.companiaFiltro = null;
+    this.filterMiembros();
+   
+  }
+
+  addFiltroProfesion() {
+    let indexFiltro = this.existFiltro("Profesion");
+    if (indexFiltro == -1) {
+      this.filtros.push({ key: "Profesion", value: this.profesionFiltro })
+    } else {
+      this.filtros[indexFiltro].value = this.profesionFiltro;
+    }
+    this.profesionFiltro = null;
+    this.filterMiembros();
+    
+  }
+
+  existFiltro(tipo: string): number {
+    let indexFiltro: number = -1;
+    this.filtros.forEach((filtro, index) => {
+      if (filtro.key === tipo) {
+        indexFiltro = index
+      }
+    })
+    return indexFiltro;
+  }
+
+  removeFilter(index: number) {
+    this.filtros.splice(index, 1)
+    if (this.filtros.length == 0) {
+      this.miembros = this.cloneMiembros;
+    }
+    else {
+      this.filterMiembros();
+    }
   }
 }
