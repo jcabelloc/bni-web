190b786c9d42aae1ec34501a55e97a2bc1c0d751
Miguel Aspilcueta <maspilcueta@itana.pe>
2020-05-28 12:08:47 -0500

    BNI-18 Listar Usuarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { AdmMiembrosComponent } from './pages/adm-miembros/adm-miembros.componen
 import { AdmGruposComponent } from './pages/adm-grupos/adm-grupos.component';
 import { AdmSesionesComponent } from './pages/adm-sesiones/adm-sesiones.component';
 import { ConsultarReferenciaComponent } from './pages/consultar-referencia/consultar-referencia.component';
+import { AdmUsuariosComponent } from './pages/adm-usuarios/adm-usuarios.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'seguridad/login', pathMatch: 'full'},
   { path:'seguridad/login', component:LoginComponent},
   { path:'main', component:BaseComponent, children:[
+    {path:'usuarios', component: AdmUsuariosComponent},
     {path:'miembros', component: AdmMiembrosComponent},
     {path:'grupos', component: AdmGruposComponent },
     {path:'sesiones', component: AdmSesionesComponent },
